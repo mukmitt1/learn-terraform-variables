2c338241ee9ebe93943bfc01fdc6dc377e68dc2f
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 21:57:22 -0500

    updated variable

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
 # Variable declarations
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
